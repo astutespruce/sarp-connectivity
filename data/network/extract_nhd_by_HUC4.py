3a26611cf7f916883bcff165431ec370841f94f9
Brendan Ward <bcward@consbio.org>
2018-11-19 17:41:45 -0800

    Improved NHD data processing

--- data/network/extract_nhd_by_HUC4.py
+++ data/network/extract_nhd_by_HUC4.py
@@ -24,7 +24,7 @@ from line_utils import to2D, calculate_sinuosity
 CRS = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
 
 # TODO: loop on this
-HUC4 = "0307"
+HUC4 = "0602"
 
 src_dir = "data/src/tmp"
 out_dir = "data/src/tmp/{}".format(HUC4)
@@ -37,32 +37,19 @@ if not os.path.exists(out_dir):
 # Read in data and convert to data frame (no need for geometry)
 start = time()
 print("Reading flowlines")
-df = gp.read_file(gdb, layer="NHDFlowline")
-
-df = df[["NHDPlusID", "FlowDir", "FType", "FCode", "geometry"]]
-# df.NHDPlusID = df.NHDPlusID.astype("int64")
+df = gp.read_file(gdb, layer="NHDFlowline")[
+    ["NHDPlusID", "FlowDir", "FType", "FCode", "GNIS_Name", "ReachCode", "geometry"]
+]
 df["id"] = df.NHDPlusID.astype("uint64")
-# df = df.set_index(["NHDPlusID"], drop=False)
 df = df.set_index(["id"])
 
 print("Read {} features".format(len(df)))
-
-print("Converting geometry to 2D")
-df.geometry = df.geometry.apply(to2D)
-
-# Filter out loops - this is not working properly for NHDPlus High resolution
-# loops = (df.StreamLeve == df.StreamCalc) & ~df.FlowDir.isnull() & (df.FType != 566)
-# df = df[~loops]
-
-
-# FIXME
-# df = df.head(100).copy()
 
 # Read in VAA and convert to data frame
 # NOTE: not all records in Flowlines have corresponding records in VAA
 print("Reading VAA table and joining...")
 vaa_df = gp.read_file(gdb, layer="NHDPlusFlowlineVAA")[
-    ["NHDPlusID", "StreamLeve", "StreamOrde", "StreamCalc"]
+    ["NHDPlusID", "StreamOrde", "StreamCalc", "TotDASqKm"]
 ]
 vaa_df.NHDPlusID = vaa_df.NHDPlusID.astype("uint64")
 vaa_df = vaa_df.set_index(["NHDPlusID"])
@@ -70,17 +57,52 @@ vaa_df = vaa_df.set_index(["NHDPlusID"])
 df = df.join(vaa_df, how="inner")  # drop any segments where we don't have info
 print("{} features after join".format(len(df)))
 
+# Filter out loops (query came from Kat).  566 is coastlines type.
+print("Filtering out loops")
+df = df.loc[
+    (df.StreamOrde == df.StreamCalc) & ~df.FlowDir.isnull() & (df.FType != 566)
+].copy()
+print("{} features after join".format(len(df)))
+
+# Calculate size classes
+print("Calculating size class")
+drainage = df.TotDASqKm
+df.loc[drainage < 10, "sizeclass"] = "1a"
+df.loc[(drainage >= 10) & (drainage < 100), "sizeclass"] = "1b"
+df.loc[(drainage >= 100) & (drainage < 518), "sizeclass"] = "2"
+df.loc[(drainage >= 518) & (drainage < 2590), "sizeclass"] = "3a"
+df.loc[(drainage >= 2590) & (drainage < 10000), "sizeclass"] = "3b"
+df.loc[(drainage >= 10000) & (drainage < 25000), "sizeclass"] = "4"
+df.loc[drainage >= 25000, "sizeclass"] = "5"
+
+
+# Convert incoming data from XYZM to XY
+print("Converting geometry to 2D")
+df.geometry = df.geometry.apply(to2D)
 
 # Project to USGS Albers CONUS so that we can calculate lengths
 print("projecting to USGS Albers")
-sinuosity_df = df[["geometry"]].to_crs(CRS)
+df = df.to_crs(CRS)
 
-print("Calculating sinuosity")
-sinuosity_df["sinuosity"] = sinuosity_df.geometry.apply(calculate_sinuosity).astype(
-    "float32"
-)
-sinuosity_df["length"] = sinuosity_df.geometry.length.astype("float32")
-df = df.join(sinuosity_df[["sinuosity", "length"]])
+# Calculate length and sinuosity
+print("Calculating length and sinuosity")
+df["length"] = df.geometry.length.astype("float32")
+df["sinuosity"] = df.geometry.apply(calculate_sinuosity).astype("float32")
+
+# Drop unneeded attributes to speed up I/O
+df = df[
+    [
+        "NHDPlusID",
+        "FType",
+        "FCode",
+        "GNIS_Name",
+        "ReachCode",
+        "sizeclass",
+        "length",
+        "sinuosity",
+        "geometry",
+    ]
+]
 
 # Write to shapefile and CSV for easier processing later
 print("Writing flowlines to disk")
