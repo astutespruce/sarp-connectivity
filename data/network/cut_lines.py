2cde97c4b11b56396a1c5230e6b657bb892f9089
Brendan Ward <bcward@consbio.org>
2018-11-20 20:05:16 -0800

    More fixes to cutting lines

--- data/network/cut_lines.py
+++ data/network/cut_lines.py
@@ -28,6 +28,7 @@ print("Reading flowline data")
 df = gp.read_file("flowline.shp")
 df.NHDPlusID = df.NHDPlusID.astype("uint64").astype("str")
 df.set_index(["NHDPlusID"], inplace=True, drop=False)
+# df["remove"] = False
 
 join_df = pd.read_csv("connections.csv")
 join_df.upstream = join_df.upstream.astype("str")
@@ -38,6 +39,7 @@ updated_joins = join_df.copy()
 # network_df = pd.read_csv("network.csv").set_index(["id"])
 
 # Read in snapped dams and waterfalls, and merge them
+# TODO: merge them before getting here
 print("Reading snapped dams and waterfalls")
 dams = pd.read_csv("snapped_dams.csv")
 dams = dams.loc[~dams.NHDPlusID.isnull()].copy()
@@ -56,27 +58,27 @@ barriers.NHDPlusID = barriers.NHDPlusID.astype("uint64").astype("str")
 geometry = [Point(xy) for xy in zip(barriers.snap_x, barriers.snap_y)]
 barriers = gp.GeoDataFrame(barriers, geometry=geometry, crs=CRS)
 
+
 # create container for new geoms
 new_flowlines = gp.GeoDataFrame(columns=df.columns, crs=df.crs, geometry="geometry")
 
 # create join table for barriers
 barrier_joins = []
 
-# Loop ovder all segments that have barriers
+# Loop over all segments that have barriers
+print("Cutting in {} barriers".format(len(barriers)))
 for idx, row in df.loc[df.index.isin(barriers.NHDPlusID.unique())].iterrows():
-    print("-----------------------\n\nNHDPlusID", idx)
-    # break
+    # print("-----------------------\n\nNHDPlusID", idx)
+
+    line = row.geometry
+    length = line.length
 
     # Find upstream and downstream segments
     upstream_ids = join_df.loc[join_df.downstream == row.NHDPlusID]
     downstream_ids = join_df.loc[join_df.upstream == row.NHDPlusID]
 
-    line = row.geometry
-    length = line.length
-
     # Barriers on this line
     points = barriers.loc[barriers.NHDPlusID == idx].copy()
-    print("barriers", points[["id", "kind"]])
 
     # ordinate the barriers by their projected distance on the line
     # Reorder this so we are always moving from upstream end to downstream end
@@ -89,7 +91,6 @@ for idx, row in df.loc[df.index.isin(barriers.NHDPlusID.unique())].iterrows():
 
     # by definition, splits must occur after the first coordinate in the line and before the last coordinate
     split_points = points.loc[(points.linepos > 0) & (points.linepos < length)]
-    # print("splits", split_points[["linepos"]])
 
     segments = []
     if len(split_points):
@@ -109,22 +110,23 @@ for idx, row in df.loc[df.index.isin(barriers.NHDPlusID.unique())].iterrows():
 
         num_segments = len(segments)
 
+        # write the new records
         for i, (segment_id, segment, barrier) in enumerate(segments):
-            new_flowlines.loc[segment_id, "SplitID"] = segment_id
+            copy_cols = list(
+                set(df.columns).difference({"geometry", "length", "sinuosity"})
+            )
+            columns = copy_cols + ["length", "sinuosity"]
+            values = [row[c] for c in copy_cols] + [
+                segment.length,
+                calculate_sinuosity(segment),
+            ]
+            new_flowlines.loc[segment_id, columns] = values
+
+            # geometry has to be written individually
             new_flowlines.loc[segment_id, "geometry"] = segment
-            new_flowlines.loc[segment_id, "length"] = segment.length
-            new_flowlines.loc[segment_id, "sinuosity"] = calculate_sinuosity(segment)
-            # copy all other cols across
-            for column in set(df.columns).difference(
-                {"geometry", "length", "sinuosity"}
-            ):
-                new_flowlines.loc[segment_id, column] = row[column]
 
             # add barrier info - barrier is None for last segment
             if barrier is not None:
-                new_flowlines.loc[segment_id, "barrier_id"] = barrier.id
-                new_flowlines.loc[segment_id, "barrier_type"] = barrier.kind
-
                 # Add segment info to the barriers join table
                 barrier_joins.append(
                     {
@@ -164,10 +166,6 @@ for idx, row in df.loc[df.index.isin(barriers.NHDPlusID.unique())].iterrows():
                     }
                 )
 
-        raise NotImplementedError(
-            "barriers on upstream coordinate of segment not yet handled!"
-        )
-
     if len(ds_points):
         # Create a record for each that has upstream set to the last segment if any, or
         # NHDPlusID of this segment
@@ -184,21 +182,24 @@ for idx, row in df.loc[df.index.isin(barriers.NHDPlusID.unique())].iterrows():
                     }
                 )
 
-        print("BREAK")
-        break
-
-    # break  # FIXME
-
+# Drop all segments replaced by new segments
+df = df.drop(df.loc[df.NHDPlusID.isin(new_flowlines.NHDPlusID)].index)
+df = df.append(new_flowlines, sort=False)
+print("Final number of segments", len(df))
 
 barrier_joins = pd.DataFrame(
     barrier_joins, columns=["NHDPlusID", "id", "upstream", "downstream"]
 )
-barrier_joins.to_csv("barrier_joins.csv", index=False)
 
+print("Writing join tables")
+barrier_joins.to_csv("barrier_joins.csv", index=False)
 updated_joins.to_csv("updated_joins.csv", index=False)
 
-new_flowlines.to_file("split_flowlines.shp", driver="ESRI Shapefile")
-new_flowlines.drop(columns=["geometry"]).to_csv("split_flowlines.csv", index_label="id")
+print("Writing split flowlines shp")
+df.to_file("split_flowlines.shp", driver="ESRI Shapefile")
+
+print("Writing split flowlines CSV")
+df.drop(columns=["geometry"]).to_csv("split_flowlines.csv", index_label="id")
 
 # barriers.to_file("barriers.shp", driver="ESRI Shapefile")
 
