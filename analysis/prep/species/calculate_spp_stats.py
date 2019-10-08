a3595d3b0831f45a43fa39404b47d5b4cdce32f0
Brendan C. Ward <bcward@astutespruce.com>
2019-10-07 18:25:51 -0700

    Refactored T&E spp stats, fixed bug with missing HUC4s

--- analysis/prep/species/calculate_spp_stats.py
+++ analysis/prep/species/calculate_spp_stats.py
@@ -11,12 +11,21 @@ Beware: Some species have incorrect spellings!  Some have many variants of commo
 
 """
 
+from pathlib import Path
+from time import time
 import pandas as pd
+from nhdnet.io import deserialize_df, serialize_df
+
+start = time()
+data_dir = Path("data")
+src_dir = data_dir / "species/source"
+out_dir = data_dir / "species/derived"
+qa_dir = data_dir / "species/qa"
 
 ############### Extract USFWS official listing information ######################
 print("Reading T&E species list")
 listed_df = pd.read_csv(
-    "data/src/ECOS_listed_aquatic_species_2018.csv",
+    src_dir / "ECOS_listed_aquatic_species_2018.csv",
     usecols=["ScientificName", "Status"],
 ).rename(columns={"ScientificName": "SNAME"})
 
@@ -30,7 +39,7 @@ listed_df = listed_df.loc[
     ~listed_df.official_status.isnull(), ["SNAME", "official_status"]
 ]
 
-# Manually add in species that have had a taxomic change (that we tripped over, NOT comprehensive)
+# Manually add in species that have had a taxanomic change (that we tripped over, NOT comprehensive)
 # Ignoring experimental population exemption here
 # Some are listed at the subspecies level by USFWS but the taxonomy at that level is not yet accepted
 
@@ -73,11 +82,12 @@ threatened_df = listed_df.loc[listed_df.official_status == "T"].SNAME.unique()
 ############## Extract species points ######################
 # Run aggregate_spp_occurrences.py first!
 print("Reading Species HUC12 data")
-df = pd.read_csv("data/src/species_HUC12.csv", dtype={"HUC12": str}).fillna(
-    {"status": ""}
-)
+df = deserialize_df(out_dir / "spp_HUC12_occurrence.feather")
+# df = pd.read_csv(out_dir / "species_HUC12.csv", dtype={"HUC12": str}).fillna(
+#     {"status": ""}
+# )
 
-print("Cleaing and aggregating data")
+print("Cleaning and aggregating data")
 
 # Fix Atlantic sturgeon (Acipenser ox*); it has multiple variants of the wrong spelling
 # There are other species that have wrong spelling, but they aren't T&E, so we aren't fixing here
@@ -92,7 +102,7 @@ df.loc[df.SNAME.isin(threatened_df) & (df.status != "E"), "status"] = "T"
 df.loc[df.SNAME.isin(endangered_df), "status"] = "E"
 
 
-### Temproary: join official T&E status in and cross-check
+### Temporary: join official T&E status in and cross-check
 # To simplify the join, apply the highest listing to all occurrences, this is the most conservative
 # Flatten to single record for each species
 # listed_df = (
@@ -109,33 +119,28 @@ df.loc[df.SNAME.isin(endangered_df), "status"] = "E"
 
 
 # At this point, assume that all T&E status is current, and aggregate up to count of unique T & E species per HUC12
-unique_species_df = (
+# Then count unique species by HUC12
+count_df = (
     df.loc[df.status.isin(["T", "E"])]
     .groupby(["HUC12", "SNAME"])
-    .size()
+    .first()
     .reset_index()
-    .drop(columns=[0])
-)
-
-count_by_HUC12_df = (
-    unique_species_df.groupby(["HUC12"])
+    .groupby(["HUC12"])
     .size()
     .reset_index()
     .rename(columns={0: "NumTEspp"})
 )
-count_by_HUC12_df.to_csv(
-    "data/summary/species_huc12_summary.csv", index=False, header=True
-)
 
+# Save counts by HUC12 to join to barriers
+serialize_df(count_df, out_dir / "spp_HUC12.feather")
 
 ## generate statistics of species and status
 species_stats_df = (
     df.loc[df.status.isin(["T", "E"])]
     # aggregate to unique species per HUC
     .groupby(["HUC12", "status", "SNAME"])
-    .size()
+    .first()
     .reset_index()
-    .drop(columns=[0])
     # aggregate count of HUCs per species
     .groupby(["status", "SNAME"])
     .size()
@@ -143,7 +148,7 @@ species_stats_df = (
     .rename(columns={0: "CountHUC12"})
 )
 
-species_stats_df.to_csv("data/src/tmp/species_stats.csv")
+species_stats_df.to_csv(qa_dir / "species_stats.csv")
 
 status_stats_df = (
     species_stats_df.groupby(["status"])
@@ -152,4 +157,6 @@ status_stats_df = (
     .rename(columns={0: "CountSpp"})
 )
 
-status_stats_df.to_csv("data/src/tmp/status_stats.csv")
+status_stats_df.to_csv(qa_dir / "status_stats.csv")
+
+print("All done in {:.2}s".format(time() - start))
