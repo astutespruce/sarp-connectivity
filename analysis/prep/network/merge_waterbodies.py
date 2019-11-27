f18d1630c581ce3203e69b047053dd38672a318b
Brendan C. Ward <bcward@astutespruce.com>
2019-11-27 11:01:58 -0800

    Rework merge of waterbodies and drain points to add global ID and retain source region

--- analysis/prep/network/merge_waterbodies.py
+++ analysis/prep/network/merge_waterbodies.py
@@ -21,6 +21,31 @@ nhd_dir = Path("data/nhd")
 start = time()
 
 
+print("Reading waterbodies...")
+wb = (
+    deserialize_gdfs(
+        [
+            nhd_dir / "waterbodies" / region / "waterbodies.feather"
+            for region in REGION_GROUPS
+        ],
+        src=[region for region in REGION_GROUPS],
+    )
+    .rename(columns={"src": "region"})
+    .reset_index(drop=True)
+)
+print("Read {:,} waterbodies".format(len(wb)))
+
+# TODO: remove on next full rerun of extract_waterbodies.py
+wb.AreaSqKm = wb.AreaSqKm.astype("float32")
+wb.FType = wb.FType.astype("uint16")
+wb.numSegments = wb.numSegments.astype("uint16")
+wb.flowlineLength = wb.flowlineLength.astype("float32")
+
+# Add a new global wbID
+wb["id"] = wb.index.copy() + 1
+wb.id = wb.id.astype("uint32")
+
+
 print("Reading flowline attributes...")
 lineAtts = deserialize_dfs(
     [nhd_dir / "flowlines" / region / "flowline.feather" for region in REGION_GROUPS],
@@ -29,48 +54,66 @@ lineAtts = deserialize_dfs(
 print("Read {:,} flowlines".format(len(lineAtts)))
 
 print("Reading waterbody drain points...")
-drains = deserialize_gdfs(
-    [
-        nhd_dir / "flowlines" / region / "waterbody_drain_points.feather"
-        for region in REGION_GROUPS
-    ]
+drains = (
+    deserialize_gdfs(
+        [
+            nhd_dir / "waterbodies" / region / "waterbody_drain_points.feather"
+            for region in REGION_GROUPS
+        ],
+        src=[region for region in REGION_GROUPS],
+        columns=[
+            "geometry",
+            "wbID",
+            "lineID",
+            "flowlineLength",
+            "numSegments",
+            "AreaSqKm",
+        ],
+    )
+    .rename(
+        columns={
+            "src": "region",
+            "wbID": "regionWBID",
+            "flowlineLength": "wbFlowlineLength",
+            "numSegments": "wbSegments",
+            "AreaSqKm": "wbAreaKM2",
+        }
+    )
+    .reset_index(drop=True)
 )
 
 print("Read {:,} waterbody drain points".format(len(drains)))
 
 # Add an internal ID, since there may be multiple drain points per waterbody
-drains["id"] = drains.index.copy()
+drains["id"] = drains.index.copy() + 1
 drains.id = drains.id.astype("uint32")
 
 # TODO: remove on next full rerun of extract_waterbodies.py
-drains.AreaSqKm = drains.AreaSqKm.astype("float32")
-drains.numSegments = drains.numSegments.astype("uint16")
+drains.regionWBID = drains.regionWBID.astype("uint32")
+drains.lineID = drains.lineID.astype("uint32")
+drains.wbFlowlineLength = drains.wbFlowlineLength.astype("float32")
+drains.wbAreaKM2 = drains.wbAreaKM2.astype("float32")
+drains.wbSegments = drains.wbSegments.astype("uint16")
 
+# join to waterbodies to get global ID
+drains = drains.join(
+    wb.set_index(["region", "wbID"]).id.rename("wbID"), on=["region", "regionWBID"]
+)
 
 # join to line atts to get NHDPlusID, etc
 drains = drains.join(lineAtts, on="lineID")
 
 
-print("Serializing waterbody drain points...")
-to_geofeather(drains, nhd_dir / "waterbody_drain_points.feather")
-serialize_sindex(drains, nhd_dir / "waterbody_drain_points.sidx")
-to_shp(drains, nhd_dir / "waterbody_drain_points.shp")
-
-
-print("Reading waterbodies...")
-wb = deserialize_gdfs(
-    [nhd_dir / "flowlines" / region / "waterbodies.feather" for region in REGION_GROUPS]
-)
-print("Read {:,} waterbodies".format(len(wb)))
-
-# TODO: remove on next full rerun of extract_waterbodies.py
-wb.AreaSqKm = wb.AreaSqKm.astype("float32")
-wb.FType = wb.FType.astype("uint16")
-wb.numSegments = wb.wbSegments.astype("uint16")
-
 print("Serializing waterbodies...")
-to_geofeather(wb, nhd_dir / "waterbodies.feather")
-to_shp(wb, nhd_dir / "waterbodies.shp")
+to_geofeather(wb, nhd_dir / "waterbodies" / "waterbodies.feather")
+
+print("Serializing waterbody drain points...")
+to_geofeather(drains, nhd_dir / "waterbodies" / "waterbody_drain_points.feather")
+serialize_sindex(drains, nhd_dir / "waterbodies" / "waterbody_drain_points.sidx")
+
+print("Serializing to shapefiles...")
+to_shp(wb, nhd_dir / "waterbodies" / "waterbodies.shp")
+to_shp(drains, nhd_dir / "waterbodies" / "waterbody_drain_points.shp")
 
 
 print("Done in {:.2f}s\n============================".format(time() - start))
