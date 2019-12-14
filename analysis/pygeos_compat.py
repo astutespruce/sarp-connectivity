29c0830232c3dea108db4ae71542e144b3345c4d
Brendan C. Ward <bcward@astutespruce.com>
2019-12-13 21:04:30 -0800

    WIP: first pass at pygeos-based spatial join

--- analysis/pygeos_compat.py
+++ analysis/pygeos_compat.py
@@ -7,17 +7,30 @@ The following operations are derived from the above PR.
 These convert data through WKB, but with NO validation (see PR for validation)
 """
 
+import numpy as np
+import pandas as pd
+import geopandas as gp
+import pygeos as pg
 from pygeos import from_wkb, to_wkb
+from pygeos.strtree import STRtree
 from shapely.wkb import loads
 from feather import read_dataframe
 
 
 def to_pygeos(geoseries):
-    return from_wkb(geoseries.apply(lambda g: g.wkb))
+    return pd.Series(from_wkb(geoseries.apply(lambda g: g.wkb)), index=geoseries.index)
 
 
 def from_pygeos(geometries):
-    return geometries.apply(lambda g: loads(to_wkb(g)))
+    def load_wkb(wkb):
+        return loads(wkb)
+
+    wkb = pg.to_wkb(geometries)
+
+    if isinstance(geometries, pd.Series):
+        return gp.GeoSeries(wkb.apply(load_wkb))
+
+    return gp.GeoSeries(np.vectorize(load_wkb, otypes=[np.object])(wkb))
 
 
 def from_geofeather_as_geos(path, columns=None):
@@ -59,3 +72,76 @@ def from_geofeather_as_geos(path, columns=None):
 
     df.geometry = from_wkb(df.geometry)
     return df
+
+
+def split_multi_geoms(series):
+    """Convert multipart geometries to a list of geometries
+
+    Parameters
+    ----------
+    series : Series
+
+    Returns
+    -------
+    Series
+
+    """
+    return series.apply(
+        lambda g: [pg.get_geometry(g, i) for i in range(0, pg.get_num_geometries(g))]
+    )
+
+
+# def cut_lines_by_points(lines, points):
+#     pass
+#     1. explode intersecting points (maybe don't?)
+#     2. calculate distance on the line
+#     3. interpolate new points from those
+#     4. sort points by distance from origin (or reverse - need to check)
+#     5. cut lines at points - need to figure out impl
+
+
+### WIP: basic STRtree based spatial join
+
+
+def query(left_geom, right, tree, predicate):
+    """Query strtree and return indices of right that intersect left
+    right, tree, predicate must be called by keyword
+    """
+    hits = tree.query(left_geom)
+    return hits[predicate(left_geom, right[hits])]
+
+
+# vectorized
+vec_query = np.vectorize(
+    query, otypes=[np.ndarray], excluded=["right", "tree", "predicate"]
+)
+
+
+def sjoin(left, right, predicate="intersects"):
+    if isinstance(left, pd.Series):
+        left_values = left.values
+        left_index = left.index
+
+    else:
+        left_values = left
+        left_index = np.arange(0, len(left))
+
+    if isinstance(right, pd.Series):
+        right_values = right.values
+        right_index = right.index
+
+    else:
+        right_values = right
+        right_index = np.arange(0, len(right))
+
+    tree = STRtree(right_values)
+    predicate_func = getattr(pg, predicate)
+
+    # hits are in 0-based indicates of right
+    hits = vec_query(
+        left_values, right=right_values, tree=tree, predicate=predicate_func
+    )
+    # need to explode and then apply indices
+    hits = pd.Series(hits, index=left_index).explode()
+    return hits.map(pd.Series(right_index))
+
