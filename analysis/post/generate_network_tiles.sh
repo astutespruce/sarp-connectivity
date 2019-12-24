d975f558a870406016057d8fd6337a1d8ebc6d4d
Brendan C. Ward <bcward@astutespruce.com>
2019-12-24 07:59:47 -0800

    Switch to GeoJSONSeq for creating tiles

--- analysis/post/generate_network_tiles.sh
+++ analysis/post/generate_network_tiles.sh
@@ -8,15 +8,17 @@ OUTDIR="tiles"
 
 BARRIER_TYPE="dams"
 
-for region in '02' '03' '05_06' '07_10' '08_11' '12' '13' '21'
+# '02'
+for region in '03' '05_06' '07_10' '08_11' '12' '13' '21'
 do
     echo "Processing ${region}"
 
     echo "Converting to GeoJSON"
-    ogr2ogr -t_srs EPSG:4326 -f GeoJSON $TMPDIR/network.json $NETWORKDIR/$region/$BARRIER_TYPE/network.shp
+    # Use GeoJSONSeq for faster parsing in tippecanoe, using the -P option
+    ogr2ogr -t_srs EPSG:4326 -f GeoJSONSeq -sql "SELECT networkID from network" $TMPDIR/network.json $NETWORKDIR/$region/$BARRIER_TYPE/network.shp
 
     echo "Creating tiles"
-    tippecanoe -f -Z 10 -z 16 -l networks -o $TILEDIR/networks$region.mbtiles $TMPDIR/network.json
+    tippecanoe -f -Z 9 -z 16 -l networks --use-attribute-for-id="networkID" -P -o $WORKDIR/networks$region.mbtiles $TMPDIR/network.json
     rm $TMPDIR/network.json
 
     echo "Done processing region"
@@ -24,12 +26,13 @@ do
     echo
 done
 
+echo "Merging regions to create final tileset"
 tile-join -f $OUTDIR/networks.mbtiles \
-    $TILEDIR/networks02.mbtiles \
-    $TILEDIR/networks03.mbtiles \
-    $TILEDIR/networks05_06.mbtiles \
-    $TILEDIR/networks07_10.mbtiles \
-    $TILEDIR/networks08_11.mbtiles \
-    $TILEDIR/networks12.mbtiles \
-    $TILEDIR/networks13.mbtiles \
-    $TILEDIR/networks21.mbtiles
+    $WORKDIR/networks02.mbtiles \
+    $WORKDIR/networks03.mbtiles \
+    $WORKDIR/networks05_06.mbtiles \
+    $WORKDIR/networks07_10.mbtiles \
+    $WORKDIR/networks08_11.mbtiles \
+    $WORKDIR/networks12.mbtiles \
+    $WORKDIR/networks13.mbtiles \
+    $WORKDIR/networks21.mbtiles
