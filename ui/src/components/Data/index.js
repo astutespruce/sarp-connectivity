02ca3b8b1cd3337bd076577a6dadd8488c2c6ee7
Brendan C. Ward <bcward@astutespruce.com>
2020-03-17 11:38:12 -0700

    Fix build error

--- ui/src/components/Data/index.js
+++ ui/src/components/Data/index.js
@@ -1,14 +1,16 @@
 import { useBoundsData } from './Bounds'
 import { useSummaryData } from './Summary'
-import {fetchBarrierInfo, fetchBarrierRanks, getDownloadURL} from './API'
+import { useStateSummary } from './StateSummary'
+import { fetchBarrierInfo, fetchBarrierRanks, getDownloadURL } from './API'
 import { Provider as BarrierTypeProvider, useBarrierType } from './BarrierType'
 
 export {
   useBoundsData,
   useSummaryData,
+  useStateSummary,
   fetchBarrierInfo,
   fetchBarrierRanks,
   BarrierTypeProvider,
   useBarrierType,
-  getDownloadURL
+  getDownloadURL,
 }
