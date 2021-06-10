51e169cd4a23166ad6cde738ece0d1562f1a5fc9
Brendan C. Ward <bcward@astutespruce.com>
2021-06-10 16:49:05 -0700

    Stub out details API endpoint

--- api/internal/__init__.py
+++ api/internal/__init__.py
@@ -3,9 +3,12 @@ from fastapi import APIRouter
 from api.internal.query import router as query_router
 from api.internal.rank import router as rank_router
 from api.internal.download import router as download_router
-
+from api.internal.details import router as details_router
 
 router = APIRouter()
 router.include_router(query_router)
 router.include_router(rank_router)
+router.include_router(details_router)
+
+# this must come last due to variable path
 router.include_router(download_router)
