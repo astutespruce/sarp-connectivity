e8ebbfba094eaf6d1eb90a40c14887bcc315a2e4
Brendan C. Ward <bcward@astutespruce.com>
2022-11-24 05:48:15 -0800

    Overhaul network tile creation to be more efficient

--- analysis/post/create_network_tiles.py
+++ analysis/post/create_network_tiles.py
@@ -5,11 +5,13 @@ from time import time
 import numpy as np
 import pandas as pd
 import geopandas as gp
+import pyarrow as pa
+import pyarrow.compute as pc
 import shapely
 from pyogrio import write_dataframe
 
-from analysis.constants import GEO_CRS, NETWORK_TYPES
-from analysis.lib.io import read_feathers
+from analysis.constants import GEO_CRS, NETWORK_TYPES, CRS
+from analysis.lib.io import read_feathers, read_arrow_tables
 from analysis.lib.geometry.lines import merge_lines
 from analysis.post.lib.tiles import get_col_types
 
@@ -18,63 +20,231 @@ intermediate_dir = Path("data/tiles")
 out_dir = Path("tiles")
 tmp_dir = Path("/tmp")
 
+# map sizeclasses (drainage area) are 1:0-2 km2, 2: 3-4, 3: 5-9, 4: 10-99, 5: 50-100, 6: 100-499, 7: 500-9999, 8: 10000 - 24999, 9: >25000 km2
+# note: need value >0 because of how these are styled in UI
+sizeclasses = [0, 2, 5, 10, 50, 100, 500, 10000, 25000]
+
 
 def classify_size(series):
-    # map sizeclasses (drainage area) are 0-2, 3-4, 5-9, 10-99, 100-499, 500-9999, 10000 - 24999, >25000 km2
-    bins = [-1, 0, 2, 5, 10, 50, 100, 500, 10000, 25000] + [
-        max(series.max(), 25000) + 1
-    ]
+    bins = sizeclasses + [max(series.max(), 25000) + 1]
     return np.asarray(
-        pd.cut(series, bins, right=False, labels=np.arange(-1, len(bins) - 2))
+        pd.cut(series, bins, right=False, labels=np.arange(1, len(bins)))
     ).astype("uint8")
 
 
 zoom_config = [
-    {"zoom": [5, 5], "size": 6, "simplification": 1000},
-    {"zoom": [6, 6], "size": 5, "simplification": 500},
-    {"zoom": [7, 7], "size": 4, "simplification": 250},
-    {"zoom": [8, 8], "size": 3, "simplification": 100},
-    {"zoom": [9, 9], "size": 2, "simplification": 50},
-    {"zoom": [10, 10], "size": 1, "simplification": 10},
-    {"zoom": [11, 16], "size": 0, "simplification": 0},
+    {
+        "zoom": [3, 4],
+        "sizeclass": 8,
+        "streamlevel": 3,
+        "simplification": 1000,
+        "scope": "national",
+    },
+    {
+        "zoom": [5, 5],
+        "sizeclass": 6,
+        "streamlevel": 5,
+        "simplification": 1000,
+        "scope": "national",
+    },
+    {
+        "zoom": [6, 6],
+        "sizeclass": 5,
+        "simplification": 500,
+        "scope": "national",
+    },
+    {
+        "zoom": [7, 7],
+        "sizeclass": 4,
+        "simplification": 250,
+        "scope": "national",
+    },
+    {
+        "zoom": [8, 8],
+        "sizeclass": 3,
+        "simplification": 100,
+        "scope": "national",
+    },
+    {
+        "zoom": [9, 9],
+        "sizeclass": 2,
+        "simplification": 50,
+        "scope": "national",
+    },
+    {"zoom": [10, 10], "sizeclass": 1, "simplification": 10},
+    {"zoom": [11, 16], "sizeclass": 0, "simplification": 0},
 ]
-
-
-zoom_levels = {
-    5: [5, 5],
-    4: [6, 6],
-    3: [6, 8],
-    2: [9, 9],
-    1: [10, 10],
-    0: [11, 16],
-}
 
 
 # use local clone of github.com/tippecanoe
 tippecanoe = "../lib/tippecanoe/tippecanoe"
 tile_join = "../lib/tippecanoe/tile-join"
 
-tippecanoe_args = [tippecanoe, "-f", "-l", "networks", "-pg", "--visvalingam"]
+col_types = [
+    "-T",
+    "dams:int",
+    "-T",
+    "small_barriers:int",
+    "-T",
+    "sizeclass:int",
+    "-T",
+    "mapcode:int",
+]
+
+tippecanoe_args = [
+    tippecanoe,
+    "-f",
+    "-l",
+    "networks",
+    "-pg",
+    "--visvalingam",
+] + col_types
+
 
 start = time()
 
 network_cols = list(NETWORK_TYPES.keys())
+merge_cols = network_cols + ["sizeclass", "mapcode"]
+
+
+print("Loading data")
 
 groups_df = pd.read_feather(src_dir / "connected_huc2s.feather")
+huc2s = sorted(groups_df.HUC2.unique())
 
-region_tiles = []
+segments = (
+    read_arrow_tables(
+        [src_dir / "clean" / huc2 / "network_segments.feather" for huc2 in huc2s],
+        columns=["lineID"] + network_cols,
+    )
+    .combine_chunks()
+    .sort_by("lineID")
+)
+
+
+flowline_paths = [src_dir / "raw" / huc2 / "flowlines.feather" for huc2 in huc2s]
+flowlines = (
+    read_arrow_tables(
+        flowline_paths,
+        columns=[
+            "lineID",
+            "intermittent",
+            "altered",
+            "TotDASqKm",
+        ],
+    )
+    .combine_chunks()
+    .sort_by("lineID")
+)
+
+# combine intermittent and altered into a single uint value
+# codes are:
+# 0: regular flowline
+# 1: intermittent flowline
+# 2: altered flowline
+# 3: altered intermittent flowline
+mapcode = np.zeros((len(flowlines),), dtype="uint8")
+mapcode[flowlines["intermittent"]] = 1
+mapcode[pc.and_not(flowlines["altered"], flowlines["intermittent"])] = 2
+mapcode[pc.and_(flowlines["altered"], flowlines["intermittent"])] = 3
+
+flowlines = pa.Table.from_pydict(
+    {
+        "lineID": flowlines["lineID"],
+        "sizeclass": classify_size(flowlines["TotDASqKm"].to_numpy()),
+        "mapcode": mapcode,
+    }
+).join(segments, "lineID")
+
+
+################## Create tiles #######################
+mbtiles_files = []
+
+### For lower zooms, build tiles across all regions for efficiency
+national_levels = [l for l in zoom_config if l.get("scope") == "national"]
+
+# read in the highest level of detail for national level and aggregate
+level = national_levels[-1]
+lines = (
+    read_arrow_tables(
+        flowline_paths,
+        columns=["lineID", "geometry", "StreamLevel"],
+        filter=pc.field("TotDASqKm") >= sizeclasses[level["sizeclass"]],
+    )
+    .join(
+        flowlines.filter(pc.field("sizeclass") >= level["sizeclass"]),
+        "lineID",
+    )
+    .to_pandas()
+)
+lines["geometry"] = shapely.from_wkb(lines.geometry.values)
+lines = gp.GeoDataFrame(lines, crs=CRS)
+
+# preliminary merge to reduce complexity
+lines = merge_lines(
+    lines,
+    by=network_cols + ["sizeclass", "mapcode", "StreamLevel"],
+).sort_values(by=network_cols)
+
+# fix dtypes after merge
+lines["sizeclass"] = lines.sizeclass.astype("uint8")
+lines["mapcode"] = lines.mapcode.astype("uint8")
+
+for level in national_levels:
+    minzoom, maxzoom = level["zoom"]
+    ix = lines.sizeclass >= level["sizeclass"]
+    if "streamlevel" in level:
+        ix = ix & (lines.StreamLevel <= level["streamlevel"])
+    subset = lines.loc[ix].copy()
+
+    print(f"Processing zooms {minzoom}-{maxzoom} ({len(subset):,} flowlines)")
+
+    # suppress styling of intermittent / altered at low zooms
+    if maxzoom < 8:
+        subset["mapcode"] = 0
+
+    subset = merge_lines(
+        subset.explode(ignore_index=True),
+        by=network_cols + ["sizeclass", "mapcode"],
+    ).sort_values(by=network_cols)
+
+    # fix dtypes after merge
+    subset["sizeclass"] = subset.sizeclass.astype("uint8")
+    subset["mapcode"] = subset.mapcode.astype("uint8")
+
+    subset["geometry"] = shapely.simplify(subset["geometry"], level["simplification"])
+
+    outfilename = tmp_dir / f"flowlines_{minzoom}_{maxzoom}.fgb"
+    mbtiles_filename = tmp_dir / f"flowlines_{minzoom}_{maxzoom}.mbtiles"
+    mbtiles_files.append(mbtiles_filename)
+
+    write_dataframe(
+        subset.to_crs(GEO_CRS),
+        outfilename,
+    )
+
+    del subset  # free up memory
+
+    ret = subprocess.run(
+        tippecanoe_args
+        + ["-Z", str(minzoom), "-z", str(maxzoom)]
+        + ["-o", f"{str(mbtiles_filename)}", str(outfilename)]
+    )
+    ret.check_returncode()
+
+    # remove FGB file
+    outfilename.unlink()
+
+
+######################
+
+
+regional_levels = [l for l in zoom_config if l.get("scope") != "national"]
 
 for group in groups_df.groupby("group").HUC2.apply(set).values:
     group = sorted(group)
 
     print(f"\n\n===========================\nProcessing group {group}")
-    segments = read_feathers(
-        [src_dir / "clean" / huc2 / "network_segments.feather" for huc2 in group],
-        columns=[
-            "lineID",
-        ]
-        + network_cols,
-    ).set_index("lineID")
 
     # create output files by HUC2 based on where the segments occur
     for huc2 in group:
@@ -82,73 +252,47 @@ for group in groups_df.groupby("group").HUC2.apply(set).values:
 
         huc2_start = time()
 
-        huc2_mbtiles_filename = intermediate_dir / f"region{huc2}_networks.mbtiles"
-        region_tiles.append(huc2_mbtiles_filename)
+        lines = (
+            pa.dataset.dataset(
+                src_dir / "raw" / huc2 / "flowlines.feather", format="feather"
+            )
+            .to_table(
+                columns=["lineID", "geometry"],
+            )
+            .join(flowlines, "lineID")
+            .to_pandas()
+        )
+        lines["geometry"] = shapely.from_wkb(lines.geometry.values)
 
-        if huc2_mbtiles_filename.exists():
-            print("already exists, skipping")
-            continue
-
-        flowlines = gp.read_feather(
-            src_dir / "raw" / huc2 / "flowlines.feather",
-            columns=[
-                "lineID",
-                "geometry",
-                "intermittent",
-                "altered",
-                "TotDASqKm",
-            ],
-        ).set_index("lineID")
-        flowlines = flowlines.join(segments)
-
-        # set custom size class for mapping
-        flowlines["sizeclass"] = classify_size(flowlines.TotDASqKm)
-
-        # combine intermittent and altered into a single uint value
-        # codes are:
-        # 0: regular flowline
-        # 1: intermittent flowline
-        # 2: altered flowline
-        # 3: altered intermittent flowline
-        flowlines["mapcode"] = 0
-        flowlines.loc[flowlines.intermittent, "mapcode"] = 1
-        flowlines.loc[(~flowlines.intermittent) & flowlines.altered, "mapcode"] = 2
-        flowlines.loc[flowlines.intermittent & flowlines.altered, "mapcode"] = 3
-        flowlines.mapcode = flowlines.mapcode.astype("uint8")
-        flowlines = flowlines.drop(columns=["intermittent", "altered"])
-
-        # aggregate to MultiLineStrings for smaller outputs
-        print("Aggregating flowlines to networks")
-        flowlines = merge_lines(
-            flowlines,
+        # aggregate by sizeclass / map code
+        lines = merge_lines(
+            gp.GeoDataFrame(lines, crs=CRS),
             by=network_cols + ["sizeclass", "mapcode"],
         ).sort_values(by=network_cols)
 
-        mbtiles_files = []
-        col_types = get_col_types(flowlines[network_cols + ["sizeclass", "mapcode"]])
+        # fix dtypes after merge
+        lines["sizeclass"] = lines.sizeclass.astype("uint8")
+        lines["mapcode"] = lines.mapcode.astype("uint8")
 
-        for level in zoom_config:
+        for level in regional_levels:
             minzoom, maxzoom = level["zoom"]
             simplification = level["simplification"]
-            size_threshold = level["size"]
+            sizeclass = level["sizeclass"]
 
             print(
-                f"Extracting size class >= {size_threshold} for zooms {minzoom} - {maxzoom} (simplifying to {simplification})"
+                f"Extracting size class >= {sizeclass} for zooms {minzoom} - {maxzoom}"
             )
-            subset = flowlines.loc[flowlines.sizeclass >= size_threshold].copy()
-
-            if maxzoom < 8:
-                # exclude altered flowlines at low zooms
-                subset = subset.loc[subset.mapcode < 2].copy()
+            subset = lines.loc[lines.sizeclass >= sizeclass].copy()
 
             if simplification:
+                print(f"simplifying to {simplification} m")
                 subset["geometry"] = shapely.simplify(
                     subset.geometry.values.data, simplification
                 )
 
-            outfilename = tmp_dir / f"region{huc2}_{minzoom}_{maxzoom}_flowlines.fgb"
+            outfilename = tmp_dir / f"region{huc2}_flowlines_{minzoom}_{maxzoom}.fgb"
             mbtiles_filename = (
-                tmp_dir / f"region{huc2}_{minzoom}_{maxzoom}_flowlines.mbtiles"
+                tmp_dir / f"region{huc2}_flowlines_{minzoom}_{maxzoom}.mbtiles"
             )
             mbtiles_files.append(mbtiles_filename)
 
@@ -163,34 +307,16 @@ for group in groups_df.groupby("group").HUC2.apply(set).values:
                 tippecanoe_args
                 + ["-Z", str(minzoom), "-z", str(maxzoom)]
                 + ["-o", f"{str(mbtiles_filename)}", str(outfilename)]
-                + col_types
             )
             ret.check_returncode()
 
             # remove FGB file
             outfilename.unlink()
 
-        print("Combining tilesets")
-        ret = subprocess.run(
-            [
-                tile_join,
-                "-f",
-                "-pg",
-                "-o",
-                str(huc2_mbtiles_filename),
-            ]
-            + [str(f) for f in mbtiles_files],
-        )
-        ret.check_returncode()
-
-        # remove temporary tilesets
-        for f in mbtiles_files:
-            f.unlink()
-
         print(f"Region done in {(time() - huc2_start) / 60:,.2f}m")
 
 
-print("\n\n============================\nCombining tiles for all regions")
+print("\n\n============================\nCombining tiles")
 mbtiles_filename = out_dir / "networks.mbtiles"
 ret = subprocess.run(
     [
@@ -200,8 +326,14 @@ ret = subprocess.run(
         "-o",
         str(mbtiles_filename),
     ]
-    + [str(f) for f in region_tiles],
+    + [str(f) for f in mbtiles_files],
 )
 ret.check_returncode()
+
+
+# Cleanup intermediate tiles
+for filename in mbtiles_files:
+    filename.unlink()
+
 
 print(f"\n\n======================\nAll done in {(time() - start) / 60:,.2f}m")
