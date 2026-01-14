f122f39bc91e81921a6f6d6bf5965f8ab17327ec
Brendan C. Ward <bcward@astutespruce.com>
2026-01-14 15:55:58 -0800

    migrate state pages

---
+++ ui/src/routes/states/[id]/+page.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+	import { createQuery } from '@tanstack/svelte-query'
+	import LoadingIcon from '@lucide/svelte/icons/loader-circle'
+
+	import { resolve } from '$app/paths'
+	import { CONTACT_EMAIL, SITE_NAME } from '$lib/env'
+	import { fetchUnitDetails } from '$lib/api'
+
+	import { Alert } from '$lib/components/alert'
+	import {
+		ActionBar,
+		BarrierStats,
+		DataProviders,
+		DownloadBar,
+		GetInvolved
+	} from '$lib/components/unitSummaryPage'
+
+	const { params, data } = $props()
+	const downloadConfig = $derived({
+		scenario: 'NCWC',
+		layer: 'State',
+		summaryUnits: { State: [params.id] }
+	})
+
+	const statsRequest = createQuery(() => ({
+		queryKey: ['State', params.id],
+		queryFn: async () => fetchUnitDetails('State', params.id)
+	}))
+
+	const stats = $derived(statsRequest.data)
+</script>
+
+<svelte:head>
+	<title>{data.name} | {SITE_NAME}</title>
+</svelte:head>
+
+<div class="container pt-8 pb-12">
+	<div class="flex gap-8 justify-between items-baseline">
+		<h1 class="leading-tight text-3xl md:text-4xl lg:text-5xl">
+			{data.name}
+		</h1>
+		<div>
+			Part of
+			{#each data.regions as region, i (region.url)}
+				{#if i > 0}
+					,
+				{/if}
+				<a href={resolve(region.url)}>
+					{region.name} region
+				</a>
+			{/each}
+		</div>
+	</div>
+
+	{#if statsRequest.error}
+		<Alert title="Oh no!" class="text-lg mt-8">
+			<p class="text-lg">
+				We're sorry, there was an unexpected error loading this page. Please reload this page in
+				your browser.
+				<br /><br />
+				If that doesn't fix the problem, please <a href={`mailto:${CONTACT_EMAIL}`}>contact us</a> to
+				let us know!
+			</p>
+		</Alert>
+	{:else if statsRequest.isLoading}
+		<div class="flex justify-center items-center gap-4 text-xl text-muted-foreground mt-12">
+			<LoadingIcon class="size-12 motion-safe:animate-spin" />
+			Loading...
+		</div>
+	{:else if statsRequest.isSuccess}
+		<DownloadBar {stats} config={downloadConfig} />
+
+		<BarrierStats areaName={data.name} map={data.map} {stats} />
+
+		<ActionBar
+			exploreURL={`/explore/?state=${params.id}`}
+			restorationURL={`/restoration/?state=${params.id}`}
+			prioritizeURL="/priority/"
+		/>
+
+		<DataProviders dataProviders={data.dataProviders} />
+
+		<GetInvolved team={data.team} />
+	{/if}
+</div>
---
+++ ui/src/routes/states/[id]/+page.ts
@@ -0,0 +1,53 @@
+import { error } from '@sveltejs/kit'
+
+import { REGIONS, STATES, CONNECTIVITY_TEAMS, STATE_DATA_PROVIDERS } from '$lib/config/constants.js'
+
+import type { EntryGenerator } from './$types'
+
+export const load = async ({ params }) => {
+	const name = STATES[params.id as keyof typeof STATES]
+
+	if (!name) {
+		error(404, `State ${params.id} not found`)
+	}
+
+	const regions = Object.values(REGIONS).filter(({ states }) => states.indexOf(params.id) !== -1)
+	const team = CONNECTIVITY_TEAMS[params.id as keyof typeof CONNECTIVITY_TEAMS]
+
+	let dataProviders = STATE_DATA_PROVIDERS[params.id as keyof typeof STATE_DATA_PROVIDERS] || []
+
+	const dataProviderFilenames = new Set(
+		Object.values(dataProviders)
+			.filter(({ logo }) => logo)
+			.map(({ logo }) => logo)
+	)
+	if (dataProviderFilenames.size > 0) {
+		const logos = Object.fromEntries(
+			Object.entries(
+				import.meta.glob('$lib/assets/images/*_logo.*', {
+					eager: true,
+					import: 'default'
+				})
+			)
+				.map(([path, img]) => [path.split('/').slice(-1)[0], img])
+				.filter(([filename]) => dataProviderFilenames.has(filename as string))
+		)
+		console.log('logos', logos)
+
+		dataProviders = dataProviders.map(({ logo, ...rest }) => ({ ...rest, logo: logos[logo] }))
+	}
+
+	const { default: map } = await import(
+		`$lib/assets/images/maps/states/${params.id}.png?as=picture&w=500;250&format=avif;webp;png`
+	)
+
+	return {
+		name,
+		regions,
+		team,
+		dataProviders,
+		map
+	}
+}
+
+export const entries: EntryGenerator = () => Object.keys(STATES).map((id) => ({ id }))
