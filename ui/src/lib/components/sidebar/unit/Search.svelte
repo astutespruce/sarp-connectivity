66961406b9c4d47d698583bf840cece26b11bb47
Brendan C. Ward <bcward@astutespruce.com>
2026-01-22 17:11:37 -0800

    WIP: add units search

---
+++ ui/src/lib/components/sidebar/unit/Search.svelte
@@ -0,0 +1,166 @@
+<script lang="ts">
+	import WarningIcon from '@lucide/svelte/icons/triangle-alert'
+	import { untrack } from 'svelte'
+	import { createQuery } from '@tanstack/svelte-query'
+
+	import { searchUnits } from '$lib/api'
+	import { LAYER_NAMES, SYSTEMS, SYSTEM_UNITS } from '$lib/config/constants'
+	import { formatNumber } from '$lib/util/format'
+
+	import SearchField from './SearchField.svelte'
+	import SearchResult from './SearchResult.svelte'
+
+	const TIMEOUT = 300 // ms
+	const MIN_QUERY_LENGTH = 3
+
+	type SearchResult = {
+		layer: string
+		id: string
+		name: string
+		bbox: string
+		state: string
+	}
+	type ResponseData = { results: SearchResult[]; remaining: number }
+
+	const { barrierType, system, layer, ignoreIds = null, showCount = false, onSelect } = $props()
+
+	let query = $state('')
+	let debouncedQuery = $state('')
+	let activeIndex: number | null = $state(null)
+	let timeout = $state()
+
+	$effect(() => {
+		query
+
+		untrack(() => {
+			clearTimeout(timeout)
+			activeIndex = null
+		})
+
+		if (!query) {
+			debouncedQuery = ''
+			return
+		}
+
+		timeout = setTimeout(() => {
+			debouncedQuery = query
+		}, TIMEOUT)
+	})
+
+	const showID = $derived(layer ? layer.startsWith('HUC') : system !== 'ADM')
+
+	const searchLabel = $derived(
+		layer
+			? LAYER_NAMES[layer as keyof typeof LAYER_NAMES].toLowerCase()
+			: SYSTEMS[system as keyof typeof SYSTEMS].toLowerCase()
+	)
+
+	const suffix = $derived(
+		` name${(system && system !== 'ADM') || (layer && layer.startsWith('HUC')) ? ' or ID' : ''}`
+	)
+
+	const searchRequest = createQuery(() => ({
+		queryKey: ['search-units', system, layer, debouncedQuery],
+		queryFn: async () => {
+			if (!(debouncedQuery && debouncedQuery.length >= MIN_QUERY_LENGTH)) {
+				return {}
+			}
+
+			const layers = layer !== null ? [layer] : SYSTEM_UNITS[system as keyof typeof SYSTEM_UNITS]
+
+			return searchUnits(layers, debouncedQuery)
+		},
+		enabled: debouncedQuery.length >= MIN_QUERY_LENGTH
+	}))
+
+	const { results = [], remaining = 0 }: ResponseData = $derived(
+		searchRequest.data || {}
+	) as ResponseData
+
+	const handleSelect = (item: SearchResult) => {
+		onSelect({
+			...item,
+			bbox: item.bbox ? item.bbox.split(',').map(parseFloat) : null
+		})
+		query = ''
+	}
+
+	const handleKeyDown = ({ key }) => {
+		if (key === 'Escape') {
+			query = ''
+			activeIndex = null
+			return
+		}
+
+		if (!(results && results.length > 0)) {
+			return
+		}
+
+		// if (key=== 'Enter')
+
+		if (key === 'ArrowUp') {
+			if (activeIndex === null) {
+				activeIndex = results.length - 1
+			} else {
+				let nextIndex = activeIndex - 1
+				if (nextIndex < 0) {
+					nextIndex = results.length - 1
+				}
+				activeIndex = nextIndex
+			}
+		} else if (key === 'ArrowDown') {
+			if (activeIndex === null) {
+				activeIndex = 0
+			} else {
+				let nextIndex = activeIndex + 1
+				if (nextIndex > results.length - 1) {
+					nextIndex = 0
+				}
+				activeIndex = nextIndex
+			}
+		}
+	}
+</script>
+
+<div>Search for a {searchLabel}:</div>
+
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+<div onkeydown={handleKeyDown} role="list">
+	<SearchField
+		bind:value={query}
+		placeholder={`${searchLabel}${suffix}`}
+		isLoading={searchRequest.isLoading}
+	/>
+
+	{#if query.length > 0 && query.length < MIN_QUERY_LENGTH}
+		<div class="italic text-muted-foreground">...keep typing...</div>
+	{:else if searchRequest.error}
+		<div class="flex gap-2 items-center text-accent">
+			<WarningIcon class="size-5" />
+			<div>Error retrieving results</div>
+		</div>
+	{:else if searchRequest.isSuccess}
+		{#if results && results.length > 0}
+			<div>
+				{#each results as result, i (result.id)}
+					<SearchResult
+						{barrierType}
+						{...result}
+						{showID}
+						disabled={ignoreIds && ignoreIds.has(result.id)}
+						focused={i === activeIndex}
+						onClick={() => handleSelect(result)}
+					/>
+				{/each}
+			</div>
+
+			{#if remaining > 0}
+				<div class="text-sm my-4 italic text-center text-muted-foreground">
+					...and {formatNumber(remaining)} more...
+				</div>
+			{/if}
+		{:else}
+			<div class="italic text-muted-foreground">No results match your search</div>
+		{/if}
+	{/if}
+</div>
